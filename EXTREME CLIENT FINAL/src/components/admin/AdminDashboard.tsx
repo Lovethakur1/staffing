87b1c4741b4ad0f814e1cd5773fe388a521c1da7
Love thakur <113324617+Lovethakur1@users.noreply.github.com>
2026-04-07 16:39:00 +0530

    update

--- EXTREME CLIENT FINAL/src/components/admin/AdminDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { ComprehensiveAdminDashboard } from "./ComprehensiveAdminDashboard";
-
-interface AdminDashboardProps {
-  adminId: string;
-}
-
-export function AdminDashboard({ adminId }: AdminDashboardProps) {
-  return <ComprehensiveAdminDashboard adminId={adminId} />;
-}
--- EXTREME CLIENT FINAL/src/components/dashboards/ClientDashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { SimplifiedClientDashboard } from "../client/SimplifiedClientDashboard";
-
-interface ClientDashboardProps {
-  userId: string;
-}
-
-export function ClientDashboard({ userId }: ClientDashboardProps) {
-  return <SimplifiedClientDashboard clientId={userId} />;
-}
